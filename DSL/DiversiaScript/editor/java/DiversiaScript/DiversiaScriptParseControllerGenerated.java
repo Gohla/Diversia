c8201c5afbfe53070f86ca0fb3f5ea1144fdee42
Gabriël Konat <gabrielkonat@gmail.com>
2011-05-16 00:39:47 +0200

    Lua code generator now generates correct code.

--- DSL/DiversiaScript/editor/java/DiversiaScript/DiversiaScriptParseControllerGenerated.java
+++ DSL/DiversiaScript/editor/java/DiversiaScript/DiversiaScriptParseControllerGenerated.java
@@ -2,6 +2,9 @@ package DiversiaScript;
 
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.imp.parser.IParseController;
 import org.strategoxt.imp.runtime.Environment;
 import org.strategoxt.imp.runtime.dynamicloading.BadDescriptorException;
@@ -30,18 +33,34 @@ public class DiversiaScriptParseControllerGenerated extends DynamicParseControll
     return descriptor;
   }
 
-  private static void createDescriptor()
+  protected static synchronized void setDescriptor(Descriptor descriptor)
+  { 
+    DiversiaScriptParseControllerGenerated.descriptor = descriptor;
+  }
+
+  protected static void createDescriptor()
   { 
     try
     { 
-      InputStream descriptorStream = DiversiaScriptParseController.class.getResourceAsStream(DESCRIPTOR);
-      InputStream table = DiversiaScriptParseController.class.getResourceAsStream(TABLE);
+      InputStream descriptorStream = DiversiaScriptParseControllerGenerated.class.getResourceAsStream(DESCRIPTOR);
+      InputStream table = DiversiaScriptParseControllerGenerated.class.getResourceAsStream(TABLE);
+      boolean filesystem = false;
+      if(descriptorStream == null && new File("./" + DESCRIPTOR).exists())
+      { 
+        descriptorStream = new FileInputStream("./" + DESCRIPTOR);
+        filesystem = true;
+      }
+      if(table == null && new File("./" + TABLE).exists())
+      { 
+        table = new FileInputStream("./" + TABLE);
+        filesystem = true;
+      }
       if(descriptorStream == null)
         throw new BadDescriptorException("Could not load descriptor file from " + DESCRIPTOR + " (not found in plugin: " + getPluginLocation() + ")");
       if(table == null)
         throw new BadDescriptorException("Could not load parse table from " + TABLE + " (not found in plugin: " + getPluginLocation() + ")");
-      descriptor = DescriptorFactory.load(descriptorStream, table, null);
-      descriptor.setAttachmentProvider(DiversiaScriptParseController.class);
+      descriptor = DescriptorFactory.load(descriptorStream, table, filesystem ? Path.fromPortableString("./") : null);
+      descriptor.setAttachmentProvider(DiversiaScriptParseControllerGenerated.class);
     }
     catch(BadDescriptorException exc)
     { 
